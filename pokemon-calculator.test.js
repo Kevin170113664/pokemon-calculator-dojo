b1e9103f894bcce92c56e125270781a0d8a91db7
Li Linhao <lhli@thoughtworks.com>
2017-05-23 01:37:18 +0800

    refactor test description

--- pokemon-calculator.test.js
+++ pokemon-calculator.test.js
@@ -2,21 +2,21 @@ const assert = require('assert');
 const type = require('./type');
 
 describe('type weakness', function () {
-    it('should weak to water when type is fire', function () {
+    it('should weak to water move when type is fire', function () {
         const fire = type.fire;
         const water = type.water;
 
         assert.equal(fire.isWeakTo(water), true);
     });
 
-    it('should weak to grass when type is water', function () {
+    it('should weak to grass move when type is water', function () {
         const water = type.water;
         const grass = type.grass;
 
         assert.equal(water.isWeakTo(grass), true);
     });
 
-    it('should weak to fire when type is grass', function () {
+    it('should weak to fire move when type is grass', function () {
         const grass = type.grass;
         const fire = type.fire;
 
