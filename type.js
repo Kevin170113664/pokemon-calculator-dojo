9b4f0d3aca50c47f3a42def936fae2cca0fda4aa
Li Linhao <lhli@thoughtworks.com>
2017-05-23 02:12:41 +0800

    refactor not effective to function

--- type.js
+++ type.js
@@ -1,19 +1,21 @@
 const _ = require('lodash');
 
+const notEffectiveChecker = (types, notEffectiveTypes) => _.every(types, type => _.includes(notEffectiveTypes, type.toString()));
+
 module.exports = {
     fire: {
         isSuperEffectiveTo: (type) => type.toString() === 'grass',
-        isNotEffectiveTo: (types) => _.every(types, type => _.includes(['fire', 'water'], type.toString())),
+        isNotEffectiveTo: (types) => notEffectiveChecker(types, ['fire', 'water']),
         toString: () => 'fire'
     },
     water: {
         isSuperEffectiveTo: (type) => type.toString() === 'fire',
-        isNotEffectiveTo: (types) => _.every(types, type => _.includes(['grass', 'water'], type.toString())),
+        isNotEffectiveTo: (types) => notEffectiveChecker(types, ['grass', 'water']),
         toString: () => 'water'
     },
     grass: {
         isSuperEffectiveTo: (type) => type.toString() === 'water',
-        isNotEffectiveTo: (types) => _.every(types, type => _.includes(['grass', 'fire'], type.toString())),
+        isNotEffectiveTo: (types) => notEffectiveChecker(types, ['grass', 'fire']),
         toString: () => 'grass'
     }
 };
