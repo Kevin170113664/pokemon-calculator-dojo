95ab40d32d99273450d16207065d0fd3fcb2e20d
Li Linhao <lhli@thoughtworks.com>
2017-05-24 22:56:56 +0800

    calculate composite types weakness

--- 2/weakness-calculator.js
+++ 2/weakness-calculator.js
@@ -1,11 +1,49 @@
+const _ = require('lodash');
+
 const type = {
-    grass: {},
-    fire: {},
-    water: {}
+    grass: {name: 'grass'},
+    fire: {name: 'fire'},
+    water: {name: 'water'},
+    flying: {name: 'flying'},
+    rock: {name: 'rock'},
+    electric: {name: 'electric'},
+    ground: {name: 'ground'},
+    bug: {name: 'bug'},
+    dark: {name: 'dark'},
+    ghost: {name: 'ghost'},
+    psychic: {name: 'psychic'},
+    ice: {name: 'ice'},
+    fighting: {name: 'fighting'}
 };
 
 type.grass.weakness = [type.fire];
-type.fire.weakness = [type.water];
+
+type.fire.weakness = [type.water, type.rock];
+type.fire.resistance = [type.grass, type.fire, type.ice];
+
 type.water.weakness = [type.grass];
 
-module.exports = {type};
+type.flying.weakness = [type.electric, type.rock, type.ice];
+type.flying.resistance = [type.grass, type.ground, type.bug, type.fighting];
+
+type.electric.weakness = [type.ground];
+
+type.psychic.weakness = [type.bug, type.dark, type.ghost];
+
+const pokemon = ({name, type1, type2}) => {
+    return {
+        name,
+        type1,
+        type2,
+        getWeakness: () => {
+            const resistance = _.uniqBy(_.concat(type1.resistance, type2.resistance), 'name');
+            const weakness = _.uniqBy(_.concat(type1.weakness, type2.weakness), 'name');
+
+            _.remove(weakness, weakness => _.includes(resistance, weakness));
+
+            return _.map(weakness, weakness => weakness.name)
+        }
+    }
+};
+
+module.exports = {type, pokemon};
--- 2/weakness-calculator.spec.js
+++ 2/weakness-calculator.spec.js
@@ -1,14 +1,21 @@
 const assert = require('assert');
-const {type} = require('./weakness-calculator');
+const _ = require('lodash');
+const {type, pokemon} = require('./weakness-calculator');
 
 describe('weakness calculator', function () {
-    it('should calculate weakness for 3 base types', function () {
-        const grass = type.grass;
-        const fire = type.fire;
-        const water = type.water;
+    it('should calculate weakness for composite types like raichu-alola', function () {
+        const raichu_alola = pokemon({type1: type.electric, type2: type.psychic});
 
-        assert.deepEqual(grass.weakness, [fire]);
-        assert.deepEqual(fire.weakness, [water]);
-        assert.deepEqual(water.weakness, [grass])
+        const weakness = raichu_alola.getWeakness();
+
+        assert.deepEqual(_.difference(weakness, ['ground', 'bug', 'dark', 'ghost']), [])
+    });
+
+    it('should calculate weakness for composite types with complementary types', function () {
+        const charizard = pokemon({type1: type.fire, type2: type.flying});
+
+        const weakness = charizard.getWeakness();
+
+        assert.deepEqual(_.difference(weakness, ['electric', 'rock', 'water']), []);
     });
 });
