c7729479e4edaef6d4d88badee8e698b21a8457c
Li Linhao <lhli@thoughtworks.com>
2017-05-30 16:41:00 +0800

    refactor composite types weakness and resistance calculation

--- 5/pokemon.js
+++ 5/pokemon.js
@@ -1,40 +1,31 @@
 const _ = require('lodash')
 
+let defaultFactor = 1;
+const defenceFactor = (types) => {
+
+  let getDefenceFactor = (type) => {
+    if (!type) return {}
+    return Object.assign({}, type.weakness, type.resistance);
+  };
+
+  let defenceFactor = _.mergeWith(getDefenceFactor(types[0]), getDefenceFactor(types[1]), (objective, source) => {
+    let objectiveFactor = (_.isNumber(objective) ? objective : defaultFactor);
+    let sourceFactor = (_.isNumber(source) ? source : defaultFactor);
+    return objectiveFactor * sourceFactor
+  });
+
+  return defenceFactor;
+}
+
 const pokemon = (...types) => {
   return {
     getWeakness: () => {
-      let defaultFactor = 1;
-
-      let getDefenceFactor = (type) => {
-        if (!type) return {}
-        return Object.assign({}, type.weakness, type.resistance);
-      };
-
-      let defenceFactor = _.mergeWith(getDefenceFactor(types[0]), getDefenceFactor(types[1]), (objective, source) => {
-        let objectiveFactor = (_.isNumber(objective) ? objective : defaultFactor);
-        let sourceFactor = (_.isNumber(source) ? source : defaultFactor);
-        return objectiveFactor * sourceFactor
-      });
-
-      return _.pickBy(defenceFactor, value => {
+      return _.pickBy(defenceFactor(types), value => {
         return value > defaultFactor
       })
     },
     getResistance: () => {
-      let defaultFactor = 1;
-
-      let getDefenceFactor = (type) => {
-        if (!type) return {}
-        return Object.assign({}, type.weakness, type.resistance);
-      };
-
-      let defenceFactor = _.mergeWith(getDefenceFactor(types[0]), getDefenceFactor(types[1]), (objective, source) => {
-        let objectiveFactor = (_.isNumber(objective) ? objective : defaultFactor);
-        let sourceFactor = (_.isNumber(source) ? source : defaultFactor);
-        return objectiveFactor * sourceFactor
-      });
-
-      return _.pickBy(defenceFactor, value => {
+      return _.pickBy(defenceFactor(types), value => {
         return value < defaultFactor
       })
     }
