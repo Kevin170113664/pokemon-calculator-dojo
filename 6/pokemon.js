f31e1cdf85210c09d591124acb90e1e492d4a93f
Li Linhao <lhli@thoughtworks.com>
2017-05-31 20:39:12 +0800

    calculate composite types pokemon resistance

--- 6/pokemon.js
+++ 6/pokemon.js
@@ -17,7 +17,18 @@ const pokemon = (...types) => {
       return _.pickBy(defence, value => value > 1)
     },
     getResistance: () => {
-      return types[0].resistance
+      const defenceFactor = type => {
+        if (!type) return {}
+        return Object.assign({}, type.resistance, type.weakness)
+      }
+
+      const defence = _.mergeWith(defenceFactor(types[0]), defenceFactor(types[1]), (obj, src) => {
+        obj = (_.isNumber(obj) ? obj : 1);
+        src = (_.isNumber(src) ? src : 1);
+        return obj * src
+      })
+
+      return _.pickBy(defence, value => value < 1)
     }
   }
 }
