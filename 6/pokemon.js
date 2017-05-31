489f7e6becea8c2b8a8f74d8ace44cf9cb83b367
Li Linhao <lhli@thoughtworks.com>
2017-05-31 20:41:25 +0800

    refactor code

--- 6/pokemon.js
+++ 6/pokemon.js
@@ -1,34 +1,27 @@
 const _ = require('lodash')
 
+const defaultFactor = 1;
+
+const getDefenceFactor = types => {
+  const defenceFactor = type => {
+    if (!type) return {}
+    return Object.assign({}, type.resistance, type.weakness)
+  }
+
+  return _.mergeWith(defenceFactor(types[0]), defenceFactor(types[1]), (obj, src) => {
+    obj = (_.isNumber(obj) ? obj : defaultFactor);
+    src = (_.isNumber(src) ? src : defaultFactor);
+    return obj * src
+  });
+};
+
 const pokemon = (...types) => {
   return {
     getWeakness: () => {
-      const defenceFactor = type => {
-        if (!type) return {}
-        return Object.assign({}, type.resistance, type.weakness)
-      }
-
-      const defence = _.mergeWith(defenceFactor(types[0]), defenceFactor(types[1]), (obj, src) => {
-        obj = (_.isNumber(obj) ? obj : 1);
-        src = (_.isNumber(src) ? src : 1);
-        return obj * src
-      })
-
-      return _.pickBy(defence, value => value > 1)
+      return _.pickBy(getDefenceFactor(types), value => value > defaultFactor)
     },
     getResistance: () => {
-      const defenceFactor = type => {
-        if (!type) return {}
-        return Object.assign({}, type.resistance, type.weakness)
-      }
-
-      const defence = _.mergeWith(defenceFactor(types[0]), defenceFactor(types[1]), (obj, src) => {
-        obj = (_.isNumber(obj) ? obj : 1);
-        src = (_.isNumber(src) ? src : 1);
-        return obj * src
-      })
-
-      return _.pickBy(defence, value => value < 1)
+      return _.pickBy(getDefenceFactor(types), value => value < defaultFactor)
     }
   }
 }
