0534103f07431711b05cd2a15563260ac095c55a
Li Linhao <lhli@thoughtworks.com>
2017-05-31 20:25:51 +0800

    calculate single type pokemon weakness

---
+++ 6/pokemon.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const {pokemon} = require('./pokemon')
+const {type} = require('./pokemon-type')
+
+describe('pokemon calculator', () => {
+
+  it('should calculate single type pokemon weakness', function () {
+    const Pikachu = pokemon(type.electric)
+    assert.deepEqual(Pikachu.getWeakness(), {ground: 2})
+
+    const Eevee = pokemon(type.normal)
+    assert.deepEqual(Eevee.getWeakness(), {fighting: 2})
+  });
+
+});
