20461318ce5aa4ffe8f9981221ec7a6354223a3d
Li Linhao <lhli@thoughtworks.com>
2017-06-01 22:02:47 +0800

    finish first test

--- 7/pokemon.test.js
+++ 7/pokemon.test.js
@@ -1,9 +1,13 @@
 const assert = require('assert')
+const {pokemon} = require('./pokemon')
+const {type} = require('./pokemon-type')
 
 describe('pokemon calculator', () => {
 
-  it('should pass', function () {
-    assert.equal(1 + 1, 2)
+  it('should calculate single type pokemon weakness', function () {
+    const Pikachu = pokemon(type.electric)
+
+    assert.deepEqual(Pikachu.getWeakness(), {ground: 2})
   });
 
 });
