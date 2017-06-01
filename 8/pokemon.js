d81947539d57733e0b4a0cd24479ea6eb0d04ea4
Li Linhao <lhli@thoughtworks.com>
2017-06-01 23:08:07 +0800

    calculate composite types pokemon weakness

--- 8/pokemon.js
+++ 8/pokemon.js
@@ -1,10 +1,23 @@
-const pokemon = (type) => {
+const _ = require('lodash')
+
+const pokemon = (...types) => {
   return {
     getWeakness: () => {
-      return type.weakness
+      const getSingleTypeDefence = function (type) {
+        if (!type) return {}
+        return Object.assign(type.weakness, type.resistance)
+      }
+
+      const defenceFactor = _.mergeWith(getSingleTypeDefence(types[0]), getSingleTypeDefence(types[1]), (obj, src) => {
+        obj = _.isNumber(obj) ? obj : 1
+        src = _.isNumber(src) ? src : 1
+        return obj * src
+      });
+
+      return _.pickBy(defenceFactor, factor => factor > 1)
     },
     getResistance: () => {
-      return type.resistance
+      return types[0].resistance
     }
   }
 }
