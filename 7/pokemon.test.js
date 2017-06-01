8acae8e82594960d6324e43413ec44ad3823d264
Li Linhao <lhli@thoughtworks.com>
2017-06-01 22:04:35 +0800

    calculate single type pokemon resistance

--- 7/pokemon.test.js
+++ 7/pokemon.test.js
@@ -6,8 +6,18 @@ describe('pokemon calculator', () => {
 
   it('should calculate single type pokemon weakness', function () {
     const Pikachu = pokemon(type.electric)
-
     assert.deepEqual(Pikachu.getWeakness(), {ground: 2})
+
+    const Eevee = pokemon(type.normal)
+    assert.deepEqual(Eevee.getWeakness(), {fighting: 2})
+  });
+
+  it('should calculate single type pokemon resistance', function () {
+    const Pikachu = pokemon(type.electric)
+    assert.deepEqual(Pikachu.getResistance(), {flying: 0.5, electric: 0.5, steel: 0.5})
+
+    const Eevee = pokemon(type.normal)
+    assert.deepEqual(Eevee.getResistance(), {ghost: 0})
   });
 
 });
