1ebe56e7939d14aa8f59704b559db5b5e2c09110
Li Linhao <lhli@thoughtworks.com>
2017-06-07 21:05:43 +0800

    refactor code

--- 10/pokemon.js
+++ 10/pokemon.js
@@ -1,36 +1,27 @@
 const _ = require('lodash')
 
+const defaultFactor = 1
+let getDefenceFactor = function (types) {
+  let _getSingleTypeDefenceFactor = (type) => {
+    if (!type) return {}
+    return Object.assign({}, type.resistance, type.weakness)
+  }
+
+  return _.mergeWith(_getSingleTypeDefenceFactor(types[0]), _getSingleTypeDefenceFactor(types[1]),
+    (obj, src) => {
+      obj = _.isNumber(obj) ? obj : defaultFactor
+      src = _.isNumber(src) ? src : defaultFactor
+      return obj * src
+    })
+}
+
 const pokemon = (...types) => {
   return {
     getWeakness: () => {
-      let _getSingleTypeDefenceFactor = (type) => {
-        if (!type) return {}
-        return Object.assign({}, type.resistance, type.weakness)
-      }
-
-      const defenceFactor = _.mergeWith(_getSingleTypeDefenceFactor(types[0]), _getSingleTypeDefenceFactor(types[1]),
-        (obj, src) => {
-          obj = _.isNumber(obj) ? obj : 1
-          src = _.isNumber(src) ? src : 1
-          return obj * src
-        })
-
-      return _.pickBy(defenceFactor, factor => factor > 1)
+      return _.pickBy(getDefenceFactor(types), factor => factor > defaultFactor)
     },
     getResistance: () => {
-      let _getSingleTypeDefenceFactor = (type) => {
-        if (!type) return {}
-        return Object.assign({}, type.resistance, type.weakness)
-      }
-
-      const defenceFactor = _.mergeWith(_getSingleTypeDefenceFactor(types[0]), _getSingleTypeDefenceFactor(types[1]),
-        (obj, src) => {
-          obj = _.isNumber(obj) ? obj : 1
-          src = _.isNumber(src) ? src : 1
-          return obj * src
-        })
-
-      return _.pickBy(defenceFactor, factor => factor < 1)
+      return _.pickBy(getDefenceFactor(types), factor => factor < defaultFactor)
     }
   }
 }
