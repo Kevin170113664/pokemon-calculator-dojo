c28c365b5155c1cb3066d5d17f536b8acff0aa00
Li Linhao <lhli@thoughtworks.com>
2017-06-07 21:04:16 +0800

    calculate composite types pokemon resistance

--- 10/pokemon.js
+++ 10/pokemon.js
@@ -18,7 +18,19 @@ const pokemon = (...types) => {
       return _.pickBy(defenceFactor, factor => factor > 1)
     },
     getResistance: () => {
-      return types[0].resistance
+      let _getSingleTypeDefenceFactor = (type) => {
+        if (!type) return {}
+        return Object.assign({}, type.resistance, type.weakness)
+      }
+
+      const defenceFactor = _.mergeWith(_getSingleTypeDefenceFactor(types[0]), _getSingleTypeDefenceFactor(types[1]),
+        (obj, src) => {
+          obj = _.isNumber(obj) ? obj : 1
+          src = _.isNumber(src) ? src : 1
+          return obj * src
+        })
+
+      return _.pickBy(defenceFactor, factor => factor < 1)
     }
   }
 }
--- 10/pokemon.test.js
+++ 10/pokemon.test.js
@@ -25,4 +25,18 @@ describe('pokemon calculator', () => {
 
     assert.deepEqual(Charizard.getWeakness(), {rock: 4, electric: 2, water: 2})
   });
+
+  it('should calculate composite types pokemon resistance', function () {
+    const Charizard = pokemon(type.fire, type.flying)
+
+    assert.deepEqual(Charizard.getResistance(), {
+      ground: 0,
+      bug: 0.25,
+      grass: 0.25,
+      fighting: 0.5,
+      steel: 0.5,
+      fairy: 0.5,
+      fire: 0.5
+    })
+  });
 });
